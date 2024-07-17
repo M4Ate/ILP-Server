c5d52b90ff262ed3c64849480222c6c0eb43675b
Max Roll <mm4ate@gmail.com>
2024-07-17 11:28:29 +0200

    Delete status enum as its not needed

--- src/main/java/com/ilp/server/Task.java
+++ src/main/java/com/ilp/server/Task.java
@@ -7,13 +7,11 @@ import org.json.JSONObject;
 public class Task {
     private final JSONObject input;
     private JSONObject output;
-    private Status status;
     private final Solver solver;
     private final HttpExchange exchange;
 
     public Task(JSONObject input, Solver solver, HttpExchange exchange) {
         this.input = input;
-        this.status = Status.PENDING;
         this.solver = solver;
         this.output = null;
         this.exchange = exchange;
@@ -23,9 +21,7 @@ public class Task {
         this.output = solver.solve(input);
     }
 
-    public Status getStatus() {
-        return status;
-    }
+
 
     public JSONObject getOutput() {
         return output;
